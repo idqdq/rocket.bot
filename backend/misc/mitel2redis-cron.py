0608cf31aee82898d3513f9c024385c07aeb7a29
Nikolay <nryzhkov@rigel.msk.vbrr.loc>
2020-10-06 12:37:36 +0300

    mitel script now sends email

--- backend/misc/mitel2redis-cron.py
+++ backend/misc/mitel2redis-cron.py
@@ -1,6 +1,7 @@
 #/usr/bin/env python3
 # the script does ssh to the PBX (Mitel) run a command (extension_info) and parses output to a dictionary
-# then put that dictionary into a Redis
+# then put that dictionary into a local Redis (@netbot port phone function to work)
+# and additionally transforms the dict to a csv file and sends that file by email
 #
 # The Output sample: 
 #
@@ -42,17 +43,20 @@ while size > 0:
     out += data.decode("utf-8") 
     size, data = channel.read()    
 channel.close()
-#print(out)
 
 baza = {}
 for num,line in enumerate(out.split('\n')):
     # filter out strings with wrong TYPE 
-    if line[11:14] != 'SIP': continue
-        
-    res = re.split(r'[:@;-]', line.split(' ')[-1])    
+    if line[11:14] != 'SIP': continue    
+    res = re.split(r'[:@;-]', line.split()[-1])
+
+    # ignore EDN (have no idea what is it anyway)
+    if res[1] == 'EDN':
+        res.pop(1)
 
     if len(res)>=3 :
         baza[res[1]] = dict(ip=res[2], mac=res[-1] if res[2] != res[-1] else '000000000000')     
+
 
 # now put it to Redis
 import redis, json
@@ -61,3 +65,52 @@ with r.pipeline() as pipe:
     for id, data in baza.items():
         pipe.set(id, json.dumps(data))
     pipe.execute()
+
+
+# create csv file
+import csv
+
+field_names = ['number', 'ip', 'mac']
+with open('/tmp/mitel_extensions_output.csv', 'w') as output:
+    writer = csv.DictWriter(output, fieldnames=field_names)
+    writer.writeheader()
+    writer.writerows([ dict(number=key, ip=val['ip'], mac=val['mac']) for key,val in baza.items()])
+
+
+# send file by email
+import email, smtplib
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+smtp_server = "algol.msk.vbrr.loc"
+sender_email = "nryzhkov@rigel.msk.vbrr.loc"
+receiver_email = "=?utf-8?B?0KDRi9C20LrQvtCyINCd0LjQutC+0LvQsNC5INCS0LjRgtCw0LvRjNC10LI=?= =?utf-8?B?0LjRhw==?= <Ryzhkov@vbrr.ru>,\
+    =?utf-8?B?0JrRg9Cx0LvQuCDQodC10YDQs9C10Lkg0JLQsNC70LXQvdGC0LjQvdC+0LI=?= =?utf-8?B?0LjRhw==?= <Kubli_SV@vbrr.ru>"
+
+subject = "Mitel Phone extensions numbers with their IP and MACs"
+body = "CSV file attached"
+
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.attach(MIMEText(body, "plain"))
+
+filename = "/tmp/mitel_extensions_output.csv" 
+with open(filename, "r") as attachment:
+    part = MIMEBase("text", "csv")
+    part.set_payload(attachment.read())
+part.add_header("Content-Disposition", f"attachment; filename= {filename}")
+message.attach(part)
+text = message.as_string()
+
+try:
+    server = smtplib.SMTP(smtp_server)
+    server.ehlo() 
+    server.sendmail(sender_email, receiver_email, text)
+    server.quit()
+except Exception as e:
+    print(e)
